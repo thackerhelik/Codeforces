20c6ce89b59c21ea9993b64e6efce0a7894eaecf
thackerhelik <thackerhelik@users.noreply.github.com>
2018-10-24 18:01:58 +0530

    Create CF1072-D2-C.cpp

---
+++ CF1072-D2-C.cpp
@@ -0,0 +1,56 @@
+/*
+
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n'
+#define fo(i,n) for(i=0;i<n;++i)
+
+#define all(x) (x).begin(),(x).end()
+#define sz(x) (int)(x).size()
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+
+typedef long long int ll;
+
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+
+    ll i, j, a, b, n, m;
+    cin >> a >> b;
+
+
+    vector<ll>f;
+    vector<ll>s;
+
+    ll sum = a + b;
+
+    ll val = 1;
+
+    while((val)*(val+1)/2 < sum)val++;
+
+    if((val)*(val+1)/2 > sum)val--;
+
+    for(i = val; i >= 1; --i){
+        if(b >= a){
+            s.pb(i);
+            b = b - i;
+        }
+        else{
+            f.pb(i);
+            a = a - i;
+        }
+    }
+
+    cout << sz(f) << endl;
+    for(auto it: f)cout << it << " ";
+    cout << endl;
+    cout << sz(s) << endl;
+    for(auto it: s)cout << it << " ";
+    cout << endl;
+    return 0;
+}
